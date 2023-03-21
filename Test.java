03559177bda529b6f38fa6992120429ce5c08d6d
prathameshkavale3004 <98210355+prathameshkavale3004@users.noreply.github.com>
2023-03-21 17:03:31 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,48 +4,59 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import java.io.IOException;
 
-public class SeleniumTestForm {      //Do not change the class name
+public class SeleniumTestForm {          //DO NOT change the class name
 
+    //Use this declarations to store respective values
     public static WebDriver driver;
+    public static String result=new String();
+    public static String msg1,msg2;
     
-    public void createDriver() {                //Do not change the method signature
-        //Implement code to create driver and assign it to 'static' driver variable	
+    
+    public void createDriver() {
+        //Implement code to create driver and assign it to 'static' driver variable
         driver=DriverSetup.getWebDriver();
         
     }
 
-    public void testSeleniumTestForm() {    //Do not change the method signature
-    	//Read the data from excel sheet. Sheet name is 'customervalid'
-       //find the elements in the form and set values parsed from excel sheet. Submit the form for registration
-       String feedData[]=null;
-       try{
-           feedData=CusRegExcel.readExcelData("CustReg.xlsx");
-       }catch(Exception e){
-           System.out.println("Error");
-       }
-       driver.findElement(By.name("cname")).sendKeys(feedData[0]);
-       driver.findElement(By.name("age")).sendKeys(feedData[1]);
-       driver.findElement(By.name("address")).sendKeys(feedData[2]);
-       driver.findElement(By.name("phonenumber")).sendKeys(feedData[3]);
-       driver.findElement(By.name("email")).sendKeys(feedData[4]);
-       driver.findElement(By.id("submit")).click();
+    public void testSeleniumTestForm(String weight,String transportMode) throws Exception{
+    	//'weight' and 'transportMode' parameter are used to send the values to the form
+        //find the elements in the form and set values as per description. Submit the form.
+        //Find the element of the message displayed after submit. Store it in 'result' variable.
+        //compare it with the message as given in the problem statement.
+        driver.findElement(By.id("weight")).sendKeys(weight);
+        if(transportMode.equals("air"))
+           driver.findElement(By.id("air")).click();
+         driver.findElement(By.id("calculate")).click();
+         
+         msg2=driver.findElement(By.id("result")).getText();
+         msg1="Dear Customer, your total shipping cost is $200";
+         if(msg1.equals(msg2))
+         {
+            result="pass";
+              
+         }else{
+             result="fail";
+         }
+         CargoShipping c=new CargoShipping();
+         c.writeExcelData("cargo.xlsx",result);
     }
     public void closeBrowser(){
         //close the browser
-        driver.quit();
+        driver.close();
     }
 
     public static void main(String[] args) throws Exception 
-	{ 
-	    SeleniumTestForm customer=new SeleniumTestForm();
-	    customer.createDriver();
-	    customer.testSeleniumTestForm();
-	    customer.closeBrowser();
-	   // CusRegExcel customer = new CusRegExcel();
-	    //Add required code
+	{ 				
+			
+		SeleniumTestForm st=new SeleniumTestForm();
+		st.createDriver();
+		st.testSeleniumTestForm("200","air");
+		st.closeBrowser();
+		//Add code here
+		
 	}
 }
