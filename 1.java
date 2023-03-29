442da5a145655b808eef163edebcba590bbfaa21
prathameshkavale3004 <98210355+prathameshkavale3004@users.noreply.github.com>
2023-03-29 19:33:06 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,72 +1,123 @@
-import static org.junit.Assert.assertEquals;
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.*;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.TestNG;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
-import org.testng.collections.Lists;
-import junit.framework.Assert;
+import org.openqa.selenium.By;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
-@Listeners(FeatureTest.class)       //DO NOT remove or alter this line. REQUIRED FOR TESTING
-public class Shipment               //DO NOT Change the class Name
+
+public class NameLocator   //DO NOT Change the class Name
 {
+	public static Document doc;
 	public static WebDriver driver;
+	public static XPath xpath;
+	public static Node node;
+	public static String fname=null;
+	public static String lname=null;
+	public static String uname=null;
+	public static String pass=null;
+	public static String msg=null;
 	
+	public static WebDriver createDriver() //DO NOT change the method signature
+	{
+	   driver=DriverSetup.getWebDriver();
+	   return driver;
+	   //Implement code to create Driver from DriverSetup, set to 'static' driver variable and return it
+	}
 	
-@BeforeSuite	//Add before suite annotation
-	public void createDriver() {           //DO NOT change the method signature
-		driver=DriverSetup.getWebDriver();//Create driver and assign it to 'static' driver variable
+	public XPath ReadFile(String xmlfileName,String id)   //DO NOT change the method signature
+    {
+        try{
+            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder=factory.newDocumentBuilder();
+            doc=builder.parse(xmlfileName);
+            }catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            doc.getDocumentElement().normalize();
+            XPathFactory xPathFactory=XPathFactory.newInstance();
+            xpath=xPathFactory.newXPath();
+            try{
+                node=(Node) xpath.compile("//User[@id="+id+"]").evaluate(doc,XPathConstants.NODE);
+                }catch(Exception e){
+                    System.out.println(e);
+                    }
+                    return xpath;
+                    //Implement code to read and return XPath object reference
     }
-	
-@DataProvider(name="shipmentData")	//Add data provider annotation with name 'shipmentData'
-	public Object[][] shipmentData()           //DO NOT change the method signature
+    public  String getFirstName(int id){
+        try{
+            node=(Node) xpath.compile("//User[@id="+id+"]/Firstname").evaluate(doc,XPathConstants.NODE);
+            fname=node.getTextContent();
+            }catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            return fname;
+            }
+            //Implement code to return firstname from xml    
+    public  String getLastName(int id){
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Lastname").evaluate(doc,XPathConstants.NODE);
+            lname=node.getTextContent();
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return lname;
+        }
+        //Implement code to return lastname from xml
+    public  String getUserName(int id){
+        try{
+            node=(Node) xpath.compile("//User[@id="+id+"]/Username").evaluate(doc,XPathConstants.NODE);
+            uname=node.getTextContent();
+        }catch (Exception e)
+        {
+         System.out.println(e);
+        }
+         return uname;
+            //Implement code to return username from xml
+    }
+    
+    public  String getPassword(int id){
+        try{
+            node=(Node) xpath.compile("//User[@id="+id+"]/Password").evaluate(doc,XPathConstants.NODE);
+            pass=node.getTextContent();
+        }catch(Exception e)
+        {
+         System.out.println(e);
+         }
+         return pass;
+         //Implement code to return passworf from xml
+    }
+     
+     public  String  getMessage(){  
+        driver.findElement(By.id("firstname")).sendKeys(getFirstName(1));
+        driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys(getLastName(1));
+        driver.findElement(By.id("username")).sendKeys(getUserName(1));
+        driver.findElement(By.id("pass")).sendKeys(getPassword(1));
+        driver.findElement(By.id("reg")).click();
+       
+        return msg="Registered Successfully";
+        //Implement code to submit form with values got from xml and return a success message.
+      }   
+      
+      public static void main(String[] args)
 	{
-	    //Do not change the values
-	    return new Object[][] { { "Mumbai", "New York","18000","10000","20000" }};
-	    
+	    NameLocator pagLocator=new NameLocator();
+	    pagLocator.createDriver();
+	    pagLocator.ReadFile("Userdetails.xml","1");
+	    System.out.println(pagLocator.getMessage());
+	    driver.close();//Implement the required code
+	    //Close the driver
 	}
-@Test(dataProvider="shipmentData")
-	//DO NOT change the method signature. Add data provider annotation with name 'shipmentData'
-	void testShipment (String orginPort,String destinationPort,String railModeCharge,String roadModeCharge,String airModeCharge) throws InterruptedException
-	{
-		Select Orgin_port=new Select(driver.findElement(By.name("origin_id")));
-		Orgin_port.selectByVisibleText(orginPort);
-		Select Destination_port=new Select(driver.findElement(By.name("destination_id")));
-		Destination_port.selectByVisibleText(destinationPort);
-		driver.findElement(By.name("submit")).click();
-		
-		Assert.assertEquals(roadModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]")).getText(),"The Road mode charge doesnt match");
-		Assert.assertEquals(railModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[3]/td[2]")).getText(),"The Rail mode charge doesnt match");
-		Assert.assertEquals(airModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[4]/td[2]")).getText(),"The Air mode charge doesnt match");
-		//Apply the array values to the respective form element
-		// Test the tabled data against the data specified in the array
-	}
-	
-	public  void shipment() {
-	    TestNG test=new TestNG();
-	    List<String>suite=Lists.newArrayList();
-	    suite.add("testng.xml");
-	    test.setTestSuites(suite);
-	    test.run();//Invoke TestNG only here
-	}
-	
-	public static void main(String[] args) {
-		Shipment ship=new Shipment();
-		ship.shipment();
-	    //Implement any required code. 
-	}
-}         
+
+}  
