2417e5e900f96946467dda911245bd2eb7a3f4fd
prathameshkavale3004 <98210355+prathameshkavale3004@users.noreply.github.com>
2023-03-29 19:21:21 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,50 +1,74 @@
- import java.io.IOException;
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Iterator;
 import java.util.List;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.TestNG;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.collections.Lists;
 
-//ADD the required TestNG annotations on respective methods
-
-public class Ex1TestNG {         //DO NOT Change the class Name
+@Listeners(FeatureTest.class)         //DO NOT remove or alter this line. REQUIRED FOR TESTING
+public class Ex2TestNG {             //DO NOT Change the class Name
 	
-	//Do NOT change these below declarations.
-	public static WebDriver driver; 
+	
+	public static WebDriver driver;
 	String name;
 	String password;
 	
+@BeforeSuite
+    public void createDriver() {                  //DO NOT change the method signature
+		//driver=DriverSetup.getWebDriver();//Create driver and assign it to 'static' driver variable
+	}
 	
-	@BeforeSuite
-	public void createDriver() {         //DO NOT change the method signature
-		driver=DriverSetup.getWebDriver();//Implement code to create Driver from DriverSetup and assign it to 'static' driver variable
-    }
-    
-	@Test(groups="init")
-	public void initializeName() {         //DO NOT change the method signature
-		name="Tom";
-	}
-	@Test(groups="init")
-	public void initializePassword() {    //DO NOT change the method signature      
-		password="Tom";
-	}
-	@Test(dependsOnGroups={"init"})
-	public void signin() {               //DO NOT change the method signature
+	@BeforeMethod
+	public void resetName() {                   //DO NOT change the method signature
+		//NO implementation required. ONLY proper annotation is required.
+	}	
+	
+	@Test(dependsOnMethods={"initializeName","initializePassword"})
+	public void signin() {                       //DO NOT change the method signature
 		System.out.println("signin");
 	}
-	@AfterSuite
-	public void closeBrowser() {         //DO NOT change the method signature
-		driver.close();
+	
+	@Test(priority=1)
+    public void initializeName() {                //DO NOT change the method signature
+		name="Tom";
 	}
 	
-	public static void main(String[] args){          //DO NOT change the method signature
-	    Ex1TestNG page=new Ex1TestNG();
-	    //Implement the required code
+	@Test(priority=0)
+	public void initializePassword() {           //DO NOT change the method signature
+		password="Tom";
 	}
+	@AfterSuite
+	public void closeBrowser() {                //DO NOT change the method signature
+		//close the driver
+	}
+	
+	
+	public void invokeTest() {                 //DO NOT change the method signature
+		TestNG test=new TestNG();
+		List<String>suite=Lists.newArrayList();
+		suite.add("testng.xml");
+		test.setTestSuites(suite);
+		test.run();//Implement code to invoke test using TestNG
+	}
+
+	public static void main(String[] args) {
+	    Ex2TestNG ex2=new Ex2TestNG();
+	    ex2.invokeTest();
+	    //Implement any required code.
+	}
+
+  
+
 } 
