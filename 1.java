78e3ea80df7fb0039ebabaaefb18ffd688d797a0
prathameshkavale3004 <98210355+prathameshkavale3004@users.noreply.github.com>
2023-03-29 19:36:17 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,122 +1,91 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 
-public class NameLocator   //DO NOT Change the class Name
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class FieldLocator //DO NOT Change the class Name
 {
-	public static Document doc;
+	JSONArray address;
 	public static WebDriver driver;
-	public static XPath xpath;
-	public static Node node;
-	public static String fname=null;
-	public static String lname=null;
-	public static String uname=null;
-	public static String pass=null;
-	public static String msg=null;
 	
 	public static WebDriver createDriver() //DO NOT change the method signature
 	{
 	   driver=DriverSetup.getWebDriver();
+	   driver.get("http://webapps.tekstac.com/AddressBook/");
 	   return driver;
-	   //Implement code to create Driver from DriverSetup, set to 'static' driver variable and return it
+	   //Implement code to create Driver from DriverSetup and set to 'static' driver variable. Return the driver.
 	}
 	
-	public XPath ReadFile(String xmlfileName,String id)   //DO NOT change the method signature
+	public JSONArray ReadFile(String fileName)   //DO NOT change the method signature
     {
         try{
-            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder=factory.newDocumentBuilder();
-            doc=builder.parse(xmlfileName);
-            }catch(Exception e)
-            {
-                System.out.println(e);
-            }
-            doc.getDocumentElement().normalize();
-            XPathFactory xPathFactory=XPathFactory.newInstance();
-            xpath=xPathFactory.newXPath();
-            try{
-                node=(Node) xpath.compile("//User[@id="+id+"]").evaluate(doc,XPathConstants.NODE);
-                }catch(Exception e){
-                    System.out.println(e);
-                    }
-                    return xpath;
-                    //Implement code to read and return XPath object reference
-    }
-    public  String getFirstName(int id){
-        try{
-            node=(Node) xpath.compile("//User[@id="+id+"]/Firstname").evaluate(doc,XPathConstants.NODE);
-            fname=node.getTextContent();
-            }catch(Exception e)
-            {
-                System.out.println(e);
-            }
-            return fname;
-            }
-            //Implement code to return firstname from xml    
-    public  String getLastName(int id){
-        try{
-            node=(Node)xpath.compile("//User[@id="+id+"]/Lastname").evaluate(doc,XPathConstants.NODE);
-            lname=node.getTextContent();
-        }catch(Exception e)
-        {
+            JSONParser parser=new JSONParser();
+            FileReader reader=new FileReader(fileName);
+            Object obj=parser.parse(reader);
+            JSONObject obj1=(JSONObject)obj;
+            JSONArray address1=(JSONArray)obj1.get("Addresses");
+            address=address1;
+        }catch(Exception e){
             System.out.println(e);
         }
-        return lname;
-        }
-        //Implement code to return lastname from xml
-    public  String getUserName(int id){
-        try{
-            node=(Node) xpath.compile("//User[@id="+id+"]/Username").evaluate(doc,XPathConstants.NODE);
-            uname=node.getTextContent();
-        }catch (Exception e)
-        {
-         System.out.println(e);
-        }
-         return uname;
-            //Implement code to return username from xml
+            return address;
+            //Implement code to read and return addresses as JSON array 
     }
     
-    public  String getPassword(int id){
-        try{
-            node=(Node) xpath.compile("//User[@id="+id+"]/Password").evaluate(doc,XPathConstants.NODE);
-            pass=node.getTextContent();
-        }catch(Exception e)
-        {
-         System.out.println(e);
-         }
-         return pass;
-         //Implement code to return passworf from xml
+    public String getNickName(int id) {
+	
+        JSONObject nickname=(JSONObject)address.get(id);
+        String nname=(String)nickname.get("NickName");
+        return nname;
+        //Implement code to return nickname from address
     }
-     
-     public  String  getMessage(){  
-        driver.findElement(By.id("firstname")).sendKeys(getFirstName(1));
-        driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys(getLastName(1));
-        driver.findElement(By.id("username")).sendKeys(getUserName(1));
-        driver.findElement(By.id("pass")).sendKeys(getPassword(1));
-        driver.findElement(By.id("reg")).click();
-       
-        return msg="Registered Successfully";
-        //Implement code to submit form with values got from xml and return a success message.
-      }   
-      
-      public static void main(String[] args)
-	{
-	    NameLocator pagLocator=new NameLocator();
-	    pagLocator.createDriver();
-	    pagLocator.ReadFile("Userdetails.xml","1");
-	    System.out.println(pagLocator.getMessage());
-	    driver.close();//Implement the required code
+
+	public String getContactName(int id) {
+		JSONObject contactname=(JSONObject)address.get(id);
+		String cname=(String)contactname.get("ContactName");
+		return cname;
+		//Implement code to return contactname from address
+	}
+
+	public String getCity(int id) {
+		JSONObject city=(JSONObject)address.get(id);
+		String cityname=(String)city.get("City");
+		return cityname;
+		//Implement code to return city from address
+	}
+
+	public String getType(int id) {
+		JSONObject type=(JSONObject)address.get(id);
+		String typ=(String)type.get("Type");
+		return typ;
+		//Implement code to return type from address
+	}
+
+	public String getMessage() {
+		driver.findElement(By.id("nickname")).sendKeys(getNickName(1));
+		driver.findElement(By.id("contact")).sendKeys(getContactName(1));
+		driver.findElement(By.id("company")).sendKeys("Cricket");
+		driver.findElement(By.id("city")).sendKeys(getCity(1));
+		driver.findElement(By.id("country")).sendKeys("India");
+		driver.findElement(By.id("type")).sendKeys(getType(1));
+		driver.findElement(By.id("add")).click();
+		String msg="Registered Successfully";
+		return msg;
+		    //Implement code to submit form with values got from json and return a success message.
+	}
+
+	public static void main(String[] args) {
+		FieldLocator pagLocator=new FieldLocator();
+	    FieldLocator.createDriver();
+	    pagLocator.ReadFile("AddressBook.json");
+	    pagLocator.getMessage();
+	    FieldLocator.driver.close();//Implement the required code
 	    //Close the driver
 	}
 
