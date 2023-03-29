2781e2ab50a61b79abcaee700641fa00ce67997c
prathameshkavale3004 <98210355+prathameshkavale3004@users.noreply.github.com>
2023-03-29 19:27:20 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,74 +1,72 @@
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.List;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.*;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.TestNG;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.collections.Lists;
+import junit.framework.Assert;
 
-@Listeners(FeatureTest.class)         //DO NOT remove or alter this line. REQUIRED FOR TESTING
-public class Ex2TestNG {             //DO NOT Change the class Name
-	
-	
+@Listeners(FeatureTest.class)       //DO NOT remove or alter this line. REQUIRED FOR TESTING
+public class Shipment               //DO NOT Change the class Name
+{
 	public static WebDriver driver;
-	String name;
-	String password;
 	
-@BeforeSuite
-    public void createDriver() {                  //DO NOT change the method signature
-		//driver=DriverSetup.getWebDriver();//Create driver and assign it to 'static' driver variable
+	
+@BeforeSuite	//Add before suite annotation
+	public void createDriver() {           //DO NOT change the method signature
+		driver=DriverSetup.getWebDriver();//Create driver and assign it to 'static' driver variable
+    }
+	
+@DataProvider(name="shipmentData")	//Add data provider annotation with name 'shipmentData'
+	public Object[][] shipmentData()           //DO NOT change the method signature
+	{
+	    //Do not change the values
+	    return new Object[][] { { "Mumbai", "New York","18000","10000","20000" }};
+	    
+	}
+@Test(dataProvider="shipmentData")
+	//DO NOT change the method signature. Add data provider annotation with name 'shipmentData'
+	void testShipment (String orginPort,String destinationPort,String railModeCharge,String roadModeCharge,String airModeCharge) throws InterruptedException
+	{
+		Select Orgin_port=new Select(driver.findElement(By.name("origin_id")));
+		Orgin_port.selectByVisibleText(orginPort);
+		Select Destination_port=new Select(driver.findElement(By.name("destination_id")));
+		Destination_port.selectByVisibleText(destinationPort);
+		driver.findElement(By.name("submit")).click();
+		
+		Assert.assertEquals(roadModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[2]/td[2]")).getText(),"The Road mode charge doesnt match");
+		Assert.assertEquals(railModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[3]/td[2]")).getText(),"The Rail mode charge doesnt match");
+		Assert.assertEquals(airModeCharge,driver.findElement(By.xpath("/html/body/table/tbody/tr[4]/td[2]")).getText(),"The Air mode charge doesnt match");
+		//Apply the array values to the respective form element
+		// Test the tabled data against the data specified in the array
 	}
 	
-	@BeforeMethod
-	public void resetName() {                   //DO NOT change the method signature
-		//NO implementation required. ONLY proper annotation is required.
-	}	
-	
-	@Test(dependsOnMethods={"initializeName","initializePassword"})
-	public void signin() {                       //DO NOT change the method signature
-		System.out.println("signin");
+	public  void shipment() {
+	    TestNG test=new TestNG();
+	    List<String>suite=Lists.newArrayList();
+	    suite.add("testng.xml");
+	    test.setTestSuites(suite);
+	    test.run();//Invoke TestNG only here
 	}
 	
-	@Test(priority=1)
-    public void initializeName() {                //DO NOT change the method signature
-		name="Tom";
-	}
-	
-	@Test(priority=0)
-	public void initializePassword() {           //DO NOT change the method signature
-		password="Tom";
-	}
-	@AfterSuite
-	public void closeBrowser() {                //DO NOT change the method signature
-		//close the driver
-	}
-	
-	
-	public void invokeTest() {                 //DO NOT change the method signature
-		TestNG test=new TestNG();
-		List<String>suite=Lists.newArrayList();
-		suite.add("testng.xml");
-		test.setTestSuites(suite);
-		test.run();//Implement code to invoke test using TestNG
-	}
-
 	public static void main(String[] args) {
-	    Ex2TestNG ex2=new Ex2TestNG();
-	    ex2.invokeTest();
-	    //Implement any required code.
+		Shipment ship=new Shipment();
+		ship.shipment();
+	    //Implement any required code. 
 	}
-
-  
-
-} 
+}         
